eacd2070a834921ec0f0b4b94255f91e880ccb49
Vorschreibung <vorschreibung@gmail.com>
2024-05-27 03:14:43 +0200

    Canonicalize release.sh
    Also appeases ShellCheck.

--- release.sh
+++ release.sh
@@ -10,13 +10,13 @@ set -Eeuo pipefail
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 # Require master
-if [ ! "$branch" = "master" ]; then
+if [[ "$branch" != "master" ]]; then
 	echo "Not in master exiting"
 	exit 1
 fi
 
 # Require the current working directory to be clean
-if [ ! -z "$(git status --porcelain)" ]; then
+if [[ -n "$(git status --porcelain)" ]]; then
 	echo "Git repository is not clean exiting"
 	exit 1
 fi
@@ -37,19 +37,19 @@ parse_params() {
 			major=$((major+1))
 			minor=0
 			patch=0
-			version_changed=true
+			version_changed=1
 			;;
 		--minor)
 			minor=$((minor+1))
 			patch=0
-			version_changed=true
+			version_changed=1
 			;;
 		--patch)
 			patch=$((patch+1))
-			version_changed=true
+			version_changed=1
 			;;
 		--sign)
-			gpg_sign=true
+			gpg_sign=1
 			;;
 		-m | --message)
 			version_message="${2-}"
@@ -68,23 +68,23 @@ parse_params() {
 parse_params "$@"
 
 # If nothing has changed then just exit
-if [ -z $version_changed ]; then
+if [[ ! $version_changed ]]; then
 	echo "Nothing to do"
 	exit 0
 fi
 
 # Sorry tag is already taken.
-if [ $(git tag -l "v$major.$minor.$patch") ]; then
+if [[ "$(git tag -l "v$major.$minor.$patch")" ]]; then
 	echo "Tag 'v$major.$minor.$patch' was taken"
 	exit 1
 fi
 
-if [ $patch = 0 ] &&  [ $(git tag -l "v$major.$minor") ]; then
+if [[ "$patch" = 0 ]] && [[ "$(git tag -l "v$major.$minor")" ]]; then
 	echo "Tag 'v$major.$minor' was taken"
 	exit 1
 fi
 
-if [ -z $version_message ]; then
+if [[ -z "$version_message" ]]; then
 	version_message="Version $major.$minor.$patch"
 fi
 
@@ -92,14 +92,13 @@ echo "Ready to commit and tag a new version: $major.$minor.$patch"
 echo "Version message will be: $version_message"
 read -p "Ready to commit? [Y/N]: " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
 	# Update the version file.
 	perl -pi -e "s/(VERSION_MAJOR)\s+[0-9]+/\1 $major/g" VERSION.txt
 	perl -pi -e "s/(VERSION_MINOR)\s+[0-9]+/\1 $minor/g" VERSION.txt
 	perl -pi -e "s/(VERSION_PATCH)\s+[0-9]+/\1 $patch/g" VERSION.txt
 
-	if [ -z $gpg_sign ]; then
+	if [[ $gpg_sign ]]; then
 		# no signing
 		# Create the release commit
 		git commit -am "Incrementing version number to $major.$minor.$patch"
@@ -117,8 +116,7 @@ then
 	echo "Committed and tagged a new release"
 	read -p "Push commit and tag to remote? [Y/N]: " -n 1 -r
 	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		git push origin master
 		git push origin "v$major.$minor.$patch"
 		echo "Pushed data to remote. Congrats!"

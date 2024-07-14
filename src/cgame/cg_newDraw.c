92c5d46cd8e7b759c10547f62668aa60243f175c
Aciz <arto.karkulahti@gmail.com>
2024-07-14 16:56:01 +0300

    cgame: set cursor status when the event handling is turned on instead
    This fixes intermission screen not initially drawing cursor, and only being visible after in-game menu was opened once.

--- src/cgame/cg_newDraw.c
+++ src/cgame/cg_newDraw.c
@@ -852,7 +852,6 @@ void CG_EventHandling(int type, qboolean fForced)
 		else if (cgs.eventHandling == CGAME_EVENT_FIRETEAMMSG)
 		{
 			cg.showFireteamMenu = qfalse;
-			cgDC.cursorVisible  = qfalse;
 			trap_Cvar_Set("cl_bypassmouseinput", "0");
 		}
 		else if (cgs.eventHandling == CGAME_EVENT_SHOUTCAST)
@@ -861,14 +860,11 @@ void CG_EventHandling(int type, qboolean fForced)
 			{
 				trap_UI_Popup(UIMENU_INGAME);
 			}
-
-			cgDC.cursorVisible = qfalse;
 			trap_Cvar_Set("cl_bypassmouseinput", "0");
 		}
 		else if (cgs.eventHandling == CGAME_EVENT_SPAWNPOINTMSG)
 		{
 			cg.showSpawnpointsMenu = qfalse;
-			cgDC.cursorVisible     = qfalse;
 			trap_Cvar_Set("cl_bypassmouseinput", "0");
 		}
 		else if (cg.snap && cg.snap->ps.pm_type == PM_INTERMISSION && fForced)

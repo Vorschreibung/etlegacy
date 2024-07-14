9395e47a13c7119fbc509c9fda425bdd920031c8
Aciz <arto.karkulahti@gmail.com>
2024-07-14 16:30:54 +0300

    mod: properly hide mouse cursor if menu bypasses mouse input

--- src/cgame/cg_newDraw.c
+++ src/cgame/cg_newDraw.c
@@ -852,6 +852,7 @@ void CG_EventHandling(int type, qboolean fForced)
 		else if (cgs.eventHandling == CGAME_EVENT_FIRETEAMMSG)
 		{
 			cg.showFireteamMenu = qfalse;
+			cgDC.cursorVisible  = qfalse;
 			trap_Cvar_Set("cl_bypassmouseinput", "0");
 		}
 		else if (cgs.eventHandling == CGAME_EVENT_SHOUTCAST)
@@ -861,11 +862,13 @@ void CG_EventHandling(int type, qboolean fForced)
 				trap_UI_Popup(UIMENU_INGAME);
 			}
 
+			cgDC.cursorVisible = qfalse;
 			trap_Cvar_Set("cl_bypassmouseinput", "0");
 		}
 		else if (cgs.eventHandling == CGAME_EVENT_SPAWNPOINTMSG)
 		{
 			cg.showSpawnpointsMenu = qfalse;
+			cgDC.cursorVisible     = qfalse;
 			trap_Cvar_Set("cl_bypassmouseinput", "0");
 		}
 		else if (cg.snap && cg.snap->ps.pm_type == PM_INTERMISSION && fForced)

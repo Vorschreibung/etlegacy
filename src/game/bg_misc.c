22399d6382f49435527c0fcd3c17d1a1faa1e470
Vorschreibung <vorschreibung@gmail.com>
2024-09-23 03:54:50 +0200

    game: Parse 'fps' in .aninc as pseudo decimal
    Pseudo decimal being a decimal with at most 2 decimal points, parsed
    into a long * 100, to ensure determinism between platforms.

--- src/game/bg_misc.c
+++ src/game/bg_misc.c
@@ -3776,7 +3776,7 @@ void BG_PlayerStateToEntityState(playerState_t *ps, entityState_t *s, int time, 
  * [0]  = names      - rank name
  * [1]  = miniNames  - mini rank name
  * [2]  = soundNames - sound to play on rank promotion
- * 
+ *
  * @todo cgame only. move ?
  */
 ranktable_t rankTable[2][NUM_EXPERIENCE_LEVELS] =
@@ -4335,6 +4335,65 @@ qboolean PC_Point_Parse(int handle, vec2_t *c)
 		}
 		(*c)[i] = f;
 	}
+	return qtrue;
+}
+
+/**
+ * @brief Parses a pseudo decimal value (with 2 dec points) into a long - for
+ * determinism reasons.
+ * @param[in] handle
+ * @param[out] l
+ * @return
+ */
+qboolean PC_PseudDec_Parse(int handle, long *l)
+{
+	pc_token_t token;
+	uint8_t    pastDot = 0;
+	uint8_t    i;
+
+	if (!trap_PC_ReadToken(handle, &token))
+	{
+		return qfalse;
+	}
+
+	for (i = 0; i < strlen(token.string); ++i)
+	{
+		if (token.string[i] == '.')
+		{
+			pastDot = 1;
+			continue;
+		}
+
+		if (pastDot > 0)
+		{
+			token.string[i - 1] = token.string[i];
+			pastDot++;
+		}
+
+		etl_assert(pastDot <= 3);
+	}
+	if (pastDot < 1)
+	{
+		pastDot++;
+	}
+
+
+	if (pastDot > 1)
+	{
+		token.string[i - 1] = '\0';
+	}
+
+	*l = strtol(token.string, NULL, 0);
+	if (l == 0)
+	{
+		*l = 1;
+	}
+
+	for (i = 0; i < (3 - pastDot); ++i)
+	{
+		*l *= 10;
+	}
+
 	return qtrue;
 }
 
